cb0ca7e94a73dcc1d30828963d28a1d34f5c8c20
Jacek programmer <jacek301@gmail.com>
2024-06-20 15:11:53 +0200

    Switch it Up!  kata solution

--- solution.rb
+++ solution.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
 def switch_it_up(number)
-  # Write your code here...
+  case number
+  when 0
+    'Zero'
+  when 1
+    'One'
+  when 2
+    'Two'
+  when 3
+    'Three'
+  when 4
+    'Four'
+  when 5
+    'Five'
+  when 6
+    'Six'
+  when 7
+    'Seven'
+  when 8
+    'Eight'
+  when 9
+    'Nine'
+  else
+    number
+
+  end
 end
