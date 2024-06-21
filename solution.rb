29db087d069db31dd3bd5423ef951fdba068d1cc
Jacek programmer <jacek301@gmail.com>
2024-06-21 16:24:26 +0200

    Find the first non-consecutive number kata solution

--- solution.rb
+++ solution.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 def first_non_consecutive(arr)
-  #your code here
+  next_element_in_order = arr[0]
+  arr.each_index do |index|
+    if index.zero?
+      next_element_in_order = arr[0]
+    else
+      next_element = arr[index]
+      next_element_in_order += 1
+      return arr[index] if next_element != next_element_in_order
+    end
+  end
+  nil
 end
