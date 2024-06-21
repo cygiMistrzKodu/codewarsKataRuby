c93a08d102f822f39bb8289931bc7893096aa0df
Jacek programmer <jacek301@gmail.com>
2024-06-21 16:30:37 +0200

    Add Length kata start

--- solution.rb
+++ solution.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-def first_non_consecutive(arr)
-  next_element_in_order = arr[0]
-  arr.each_index do |index|
-    if index.zero?
-      next_element_in_order = arr[0]
-    else
-      next_element = arr[index]
-      next_element_in_order += 1
-      return arr[index] if next_element != next_element_in_order
-    end
-  end
-  nil
+def add_length(str)
+  # your code here
 end
--- tests_spec.rb
+++ tests_spec.rb
@@ -2,14 +2,11 @@
 
 require_relative 'solution'
 
-describe 'first_non_consecutive' do
-  it 'powinno działać dla stałych testów' do
-    expect(first_non_consecutive([1, 2, 3, 4, 6, 7, 8])).to eq(6)
-    expect(first_non_consecutive([1, 2, 3, 4, 5, 6, 7, 8])).to be_nil
-    expect(first_non_consecutive([4, 6, 7, 8, 9, 11])).to eq(6)
-    expect(first_non_consecutive([4, 5, 6, 7, 8, 9, 11])).to eq(11)
-    expect(first_non_consecutive([31, 32])).to be_nil
-    expect(first_non_consecutive([-3, -2, 0, 1])).to eq(0)
-    expect(first_non_consecutive([-5, -4, -3, -1])).to eq(-1)
+describe('Add Length') do
+  it('Sample Tests') do
+    expect(add_length('apple ban')).to eq(['apple 5', 'ban 3'])
+    expect(add_length('you will win')).to eq(['you 3', 'will 4', 'win 3'])
+    expect(add_length('you')).to eq(['you 3'])
+    expect(add_length('y')).to eq(['y 1'])
   end
 end
